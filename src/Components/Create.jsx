0867d83f15c46aa9ba3b60374a8352e51b573bbd
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-04 18:26:44 +0530

    Post Api integrated

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Create = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const header = {"Access-Control-Allow-Origin": "*"}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('data sent')
+    axios.post("https://64033a83302b5d671c49f1d7.mockapi.io/practise", {
+      name: name,
+      email: email,
+        header,
+    });
+  };
   return (
     <>
       <form>
         <div className="mb-3">
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-          <label for="exampleInputEmail1" className="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
@@ -23,12 +38,14 @@ const Create = () => {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
           </div>
         </div>
-       
+
         {/* <div className="mb-3 form-check">
           <input
             type="checkbox"
@@ -39,7 +56,11 @@ const Create = () => {
             Check me out
           </label>
         </div> */}
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </form>
