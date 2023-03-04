52176cdd92399df91d3b2d5c293d645962616d3a
Bidhu1024 <bidhu1024av@gmail.com>
2023-03-04 18:07:02 +0530

    Form struc complete

---
+++ src/Components/Create.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Create = () => {
+  return (
+    <>
+      <form>
+        <div className="mb-3">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+       
+        {/* <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div> */}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Create;
